af3406ea306e14338258c060b8f55dee7e491898
Rivan Dwi Arhiansyah <rivandw@gmail.com>
2021-01-27 19:28:35 +0700

    Upload New Project Login Page + Splash Screen

---
+++ MyLoginPage/app/src/main/java/com/rivandwiarhiansyah/myloginpage/LoginPage.java
@@ -0,0 +1,18 @@
+package com.rivandwiarhiansyah.myloginpage;
+//credit by Developer Wetravel
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+
+        //OTW buat Intent
+    }
+}
